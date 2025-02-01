1a4367a32332f880fbf4df49767e1484660d9381
Raul <ralpesinterventor@gmail.com>
2025-02-01 09:21:40 -0500

    se realizaron cambios

--- Pokemon js 2da prueva/pokemon 2da prueva.js
+++ Pokemon js 2da prueva/pokemon 2da prueva.js
@@ -1,5 +1,12 @@
-function seleccionarMascotaJugador(){
-    alert("seleccionaste tu mascota")
-}
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+document.getElementById("boton-mascota").addEventListener("click", function () {
+    const mascotaSeleccionada = document.querySelector('input[name="mascota"]:checked');
+    if (mascotaSeleccionada) {
+        alert("Has seleccionado: " + mascotaSeleccionada.id);
+    } else {
+        alert("Por favor selecciona una mascota.");
+    }
+});
+document.getElementById("boton-reiniciar").addEventListener("click", function () {
+    location.reload(); // Recarga la página
+});
